7d3062368b7e36e153b7d700caffe26725f112f6
Federico Lombardozzi <lombardozzifederico@outlook.com>
2021-06-26 20:58:29 -0300

    tabs fix

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,37 +1,66 @@
-import { useState, useEffect, Children } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import styles from "./Tabs.module.css";
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import { HStack, Box, Button } from "@chakra-ui/react";
 
 import { slugify } from "../../utils/slugify";
 
-const Tabs = () => {
+const Tabs = ({ children, initialTab }) => {
+  const [activeTab, setActiveTab] = useState(children[0].props.label);
+  const router = useRouter();
+
+  const handleClick = (e, newActiveTab) => {
+    e.preventDefault();
+    setActiveTab(slugify(newActiveTab));
+  };
+
+  useEffect(() => {
+    if (initialTab.tab) {
+      setActiveTab(initialTab.tab);
+      console.log(initialTab);
+    }
+  }, []);
+
+  useEffect(() => {
+    router.push(`${router.pathname}?tab=${slugify(activeTab)}`, undefined, {
+      shallow: true,
+    });
+    console.log(activeTab);
+  }, [activeTab]);
+
   return (
-    <div>
-      <Tabs defaultIndex={1}>
-        <TabPanels>
-          <TabPanel>
-            <Image
-              boxSize="200px"
-              fit="cover"
-              src="https://resizing.flixster.com/wTgvsiM8vNLhCcCH-6ovV8n5z5U=/300x300/v1.bjsyMDkxMzI5O2o7MTgyMDQ7MTIwMDsxMjAwOzkwMA"
-            />
-          </TabPanel>
-          <TabPanel>
-            <Image
-              boxSize="200px"
-              fit="cover"
-              src="https://vignette.wikia.nocookie.net/naruto/images/2/21/Sasuke_Part_1.png/revision/latest?cb=20170716092103"
-            />
-          </TabPanel>
-        </TabPanels>
-        <TabList>
-          <Tab>Naruto</Tab>
-          <Tab>Sasuke</Tab>
-        </TabList>
-      </Tabs>
-    </div>
+    <Box mr={{ base: 0, md: 200 }} ml={{ md: 200 }}>
+      <HStack spacing="16px" p="16px">
+        {children.map((tab) => {
+          const label = tab.props.label;
+          return (
+            <Box
+              w="100%"
+              h="150px"
+              align="center"
+              borderWidth="1px"
+              borderRadius="lg"
+              overflow="hidden"
+              _hover={{ bg: "teal.400" }}
+              onClick={(e) => handleClick(e, label)}
+              className={slugify(label) == activeTab ? styles.current : ""}
+              key={label}
+            >
+              <a href="#">{label}</a>
+            </Box>
+          );
+        })}
+      </HStack>
+      {children.map((one) => {
+        if (slugify(one.props.label) == activeTab)
+          return (
+            <div key={one.props.label} className={styles.content}>
+              {one.props.children}
+            </div>
+          );
+      })}
+    </Box>
   );
 };
 
-export default Tabs;
+export { Tabs };
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Link from "next/link";
 import Head from "../components/head";
 import Nav from "../components/nav";
+import { Tabs } from "../components/Tabs";
 
-export default function Home() {
+export default function Home({ query }) {
   return (
     <div>
       <Head title="Home" />
@@ -10,7 +11,41 @@ export default function Home() {
       <div className="hero">
         <h4 className="title">Which service(s) do you need?</h4>
       </div>
-
+      <Tabs initialTab={query}>
+        <div label="Tab 1">
+          <h2>Tab 1</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+        <div label="Tab 2">
+          <h2>Tab 2</h2>
+          <p>
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </p>
+        </div>
+        <div label="Tab 3">
+          <h2>Tab 3</h2>
+          <p>
+            Sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem
+            ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Dolore eu fugiat nulla pariatur. Excepteur
+            sint occaecat cupidatat non proident, sunt in culpa qui officia
+            deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </Tabs>
       <style jsx>{`
         .title {
           margin: 0;
@@ -28,3 +63,7 @@ export default function Home() {
     </div>
   );
 }
+
+Home.getInitialProps = ({ query }) => {
+  return { query };
+};
